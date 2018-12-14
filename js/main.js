c0ddb03e698f4cdfb033b312e9ec6bc5c841b2d6
Robbie <saucy.cooking@gmail.com>
2018-12-14 12:47:52 -0700

    Commit 1 - Q&A

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// 1. const or let = "value"
+
+// 2. variable = 'new definition' - must be a variable declared by using let to be able to change it.
+
+// 3. variable = newVariable
+
+// 4. declare variable is when you first make a variable like: const variableName,  you do not need to give it an initial value.  
+//  assign allows you to set a specific value to the variable.  Define a variable refers to assigning a value to a variable during initialization or otherwise.
+
+// 5. Pseudocoding is a strategy to plan out your code by writing down in english step by step logic of what you are going to do.
+//  you should do this because it will help you become more efficient and better at coding.
+
+// 6. 70 - 80% of time should be spent planning how you are going to solve a problem.
